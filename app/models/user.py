fa0bdc6817db637c754619239f9c3cd083554476
Gideon Arianne E. Basco <gaebasco@FDA-Central.local>
2026-01-15 10:43:08 +0800

    Update Users table and add new table named group

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,17 @@
 from sqlalchemy.sql import func
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    Enum,
+    ForeignKey
+)
+from sqlalchemy.orm import relationship
 from app.db.base_class import Base
 import enum
+
 
 # Define Role Enum
 class UserRole(str, enum.Enum):
@@ -9,27 +19,32 @@ class UserRole(str, enum.Enum):
     ADMIN = "Admin"
     SUPERADMIN = "SuperAdmin"
 
+
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
-    
+
     # login identifiers
     email = Column(String(255), nullable=False, unique=True, index=True)
     username = Column(String(100), nullable=False, unique=True, index=True)
     hashed_password = Column(String(255), nullable=False)
-    
+
     # personal info
     first_name = Column(String(100), nullable=False)
     surname = Column(String(100), nullable=False)
     position = Column(String(100), nullable=True)
-    
+
     # role
     role = Column(Enum(UserRole), default=UserRole.USER, nullable=False)
-    
+
+    # ✅ group (NEW)
+    group_id = Column(Integer, ForeignKey("groups.id"), nullable=False)
+    group = relationship("Group")
+
     # status
     is_active = Column(Boolean, default=True)
-    
+
     # timestamps
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
